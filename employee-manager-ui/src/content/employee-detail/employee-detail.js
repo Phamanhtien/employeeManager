cc7a9789f9e5530c850039ce09721d5cbca2726d
Phamanhtien <48859143+Phamanhtien@users.noreply.github.com>
2022-04-19 17:58:17 +0700

    Update employee-detail.js

--- employee-manager-ui/src/content/employee-detail/employee-detail.js
+++ employee-manager-ui/src/content/employee-detail/employee-detail.js
@@ -18,7 +18,6 @@ import "./employee-detail.css";
 import Employee from "../../model/employee";
 
 function EmployeeDetail() {
-    console.log("Employee trong detail " + EmployeeState.employee.id)
     let pathname = window.location.pathname.split("/");
     let employeeId = pathname[pathname.length - 1];
 
@@ -28,17 +27,22 @@ function EmployeeDetail() {
     const { status, data } = useQuery("employeeByIdQuery", () =>
         RetrieveEmployee(employeeId)
     );
-    
+
     let navigate = useNavigate();
 
-    console.log(EmployeeState.employee)
-
-    if (EmployeeState.employee.id !== data.id) {
-        EmployeeState.employee = data;
-        return <Loading></Loading>
+    if (status === "loading") {
+        return <Loading></Loading>;
     }
 
-    console.log("Employee trong detail 2 " + EmployeeState.employee.id)
+    if (EmployeeState.employee === undefined) {
+        return <Loading></Loading>;
+    }
+
+    if (EmployeeState.employee.id === undefined) {
+        EmployeeState.employee = data;
+        return <Loading></Loading>;
+    }
+
     function updateEmployee(employee) {
         UpdateEmployee(employee).then((res) => {
             if (res === "") {
